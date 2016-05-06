19a971b5c7a047dc1e4085a30ff6dfa6d3a44ead
TiboDica <priou.thibaud@laposte.net>
2016-05-06 12:13:21 +0200

    finition de register

--- register.php
+++ register.php
@@ -4,15 +4,11 @@ session_start();
 $connect = false;
 $emailDuplicatedError = NULL;
 $passwordsDifferentError = NULL;
+$disconnectMsg = NULL;
 
 include("boilerplate.php");
 
-if ($connect) {
-	include("navbar_connected.php");
-	echo "<div class='alert alert-info' role=alert>Your account has been successfully created.</div>";
-} else {
-	include("navbar_unconnected.php");	
-	if (isset($_POST['edit'])) {
+if (isset($_POST['edit'])) {
 		$name = $_POST['name'];
 		$firstname = $_POST['firstname'];
 		$pseudo = $_POST['pseudo'];
@@ -34,77 +30,83 @@ if ($connect) {
 			$connect = true;
 			$_SESSION['email'] = $_POST['email'];
 		}
-	} elseif (isset($_SESSION['connect'])) {
-		echo "<div>You have been disconnected.</div>";
-		session_destroy();
-	}	
+} elseif (isset($_SESSION['connect'])) {
+	$disconnectMsg = "You have been disconnected.";
+	session_destroy();
+}	
+	
+if ($connect) {
+	include("navbar_connected.php");
+	?>
+	<div class='container'>
+		<div class='alert alert-info  col-sm-9 col-sm-offset-2' role=alert>Your account has been successfully created.</div>";
+	</div>
+<?php
+} else {
+	include("navbar_unconnected.php");	
 	?>	
 	<div class="container">
-		<form class="form-vertical" action="register.php" method='post'>
-		
-			<!------- Name ------->		
+		<?php
+		if (isset($disconnectMsg))
+			echo "<div class='alert alert-info  col-sm-9 col-sm-offset-2' role=alert>".$disconnectMsg."</div>";
+		?>
+		<form class="form-horizontal" action="register.php" method='post'>
+			<!-- Name -->		
 			<div class="form-group">
 				<label for="name" class="control-label col-sm-2">Name</label>
 				<div class="col-sm-9">
 					<input type="text" class="form-control" name="name" required title="This field is required" placeholder="Enter your name"
 						<?php
-							if (isset($edit)) echo "value=$name";
+							if (isset($edit)) echo "value=\"$name\"";
 						?>
 					>
 				</div>
 			</div>
-			<br>
 				
-			<!------- First Name ------->
+			<!-- First Name -->
 			<div class="form-group">
 				<label for="firstname" class="control-label col-sm-2">First Name</label>
 				<div class="col-sm-9">
 					<input type="text" class="form-control" name="firstname" required title="This field is required" placeholder="Enter your firstname"
 						<?php
-							if (isset($edit)) echo "value=$firstname";
+							if (isset($edit)) echo "value=\"$firstname\"";
 						?>
 					>
 				</div>
-			</div>			
-			<br>
+			</div>
 		
-			<!------- Pseudo ------->
+			<!-- Pseudo -->
 			<div class="form-group">
 				<label for="pseudo" class="control-label col-sm-2">Pseudo</label>
 				<div class="col-sm-9">
 					<input type="text" class="form-control" name="pseudo" required pattern="[a-zA-Z0-9]{2,}" title="Valid pseudo contains at least 2 characters or numbers" placeholder="Valid pseudo contains at least 2 characters or numbers"
 						<?php
-						if (isset($edit)) echo "value=$pseudo";
+						if (isset($edit)) echo "value=\"$pseudo\"";
 						?>
 					>
 				</div>
-			</div>			
-			<br>
+			</div>
 			
-			<!------- Email ------->		
+			<!-- Email -->		
 			<div class="form-group">
 				<?php
-					if (isset($emailDuplicatedError)) {
-						echo "<div class='alert alert-danger'>$emailDuplicatedError</div>";
-					}
+					if (isset($emailDuplicatedError)) echo "<div class='alert alert-danger col-sm-9 col-sm-offset-2'>$emailDuplicatedError</div>";
 				?>
 				<label for="email" class="control-label col-sm-2">Email</label>
 				<div class="col-sm-9">
 					<input type="email" class="form-control" name="email" required pattern="[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}" title="Please enter a valid email address" placeholder="Please enter a valid email address"
 						<?php
-						if (isset($edit)) echo "value=$email";
+						if (isset($edit)) echo "value=\"$email\"";
 						?>
 					>
 				</div>
 			</div>
-			<br>
 
-
-			<label for="Address" class="">Address</label>
-			<!------- Street ------->				
+			<!-- Street -->				
 			<div class=form-group>					
+				<label for="Adresse" class="control-label col-sm-2">Address</label>
 				<div class="col-sm-9">
-					<input type="text" class="form-control" name="street" required placeholder="Enter street numer and name"
+					<input type="text" class="form-control" name="street" required placeholder="Enter street number and name"
 						<?php
 							if (isset($edit)) echo "value=\"$street\"";
 						?>
@@ -112,70 +114,66 @@ if ($connect) {
 				</div>
 			</div>
 			
-			<!----- ZIP Code----->
+			<!-- City and ZIP Code -->
 			<div class=form-group>	
-				<div class="col-sm-9">
+				<label for="City" class="control-label col-sm-2">City</label>
+				<div class="col-sm-3">
 					<input type="number" class="form-control" name="zip_code" required placeholder="ZIP code"
 						<?php
 							if (isset($edit)) echo "value=$zip_code";
 						?>
 					>
 				</div>
-			</div>		
-			
-			<!----- City ------->
-			<div class=form-group>
-				<div class="col-sm-9">
+				<div class="col-sm-6">
 					<input type="text" class="form-control" name="city" required placeholder="City"
 						<?php
-							if (isset($edit)) echo "value=$city";
+							if (isset($edit)) echo "value=\"$city\"";
 						?>
 					>
 				</div>
 			</div>
-			<!----- Country ----->
+			
+			<!-- Country -->
 			<div class=form-group>
+				<label for="country" class="control-label col-sm-2">Country</label>
 				<div class="col-sm-9">
 					<input type="text" class="form-control" name="country" required placeholder="Country"
 						<?php
-							if (isset($edit)) echo "value=$country";
+							if (isset($edit)) echo "value=\"$country\"";
 						?>
 					>
 				</div>
 			</div>
-			<br>
 		
-			<!------- Password ------->
+			<!-- Password -->
 			<div class="form-group">
 				<?php
-					if (isset($passwordsDifferentError)) echo "<div class ='alert alert-danger'>$passwordsDifferentError</div>";
+					if (isset($passwordsDifferentError)) echo "<div class='alert alert-danger col-sm-9 col-sm-offset-2'>$passwordsDifferentError</div>";
 				?>	
 				<label for="first_name" class="control-label col-sm-2">Password</label>
 				<div class="col-sm-9">
 					<input type="password" class="form-control" name="pwd1" pattern=".{6,}" placeholder="Valid password must be of 6 characters minimum" required title="Valid password must be of 6 characters minimum">
 				</div>
 			</div>
-			<br>
 			
-			<!------- Password (confirm) ------->
+			<!-- Confirm Password-->
 			<div class="form-group">
-				<label for="first_name" class="control-label col-sm-2">(confirm) Password</label>
-				<div class="col-sm-9">
-					<input type="password" class="form-control" name="pwd2" pattern=".{6,}" placeholder="Valid password must be of 6 characters minimum" required title="Valid password must be of 6 characters minimum">
+				<div class="col-sm-offset-2 col-sm-9">
+					<input type="password" class="form-control" name="pwd2" pattern=".{6,}" placeholder="Confirm password" required title="Valid password must be of 6 characters minimum">
 				</div>
 			</div>
-			<br>
-				
-			<div class="form-group">
-				<input type="hidden" class="form-control" name="edit">
-			</div>
 					
+			<!-- Submit -->		
 			<div class="form-group">
-				<div class="col-sm-2">
+				<div class="col-sm-offset-2 col-sm-10">
 					<button type="submit" formaction='register.php' class="btn btn-default">Sign in</button>
 				</div>
 			</div>
-			<br>
+			
+			<!-- Hidden -->
+			<div class="form-group">
+				<input type="hidden" class="form-control" name="edit">
+			</div>
 			
 		</form>
 	</div>
